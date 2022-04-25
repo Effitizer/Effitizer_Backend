e2c1c4e650d48a03c23aabe5dcdcb4a4e3532398
chea-young <63410509+chea-young@users.noreply.github.com>
2022-04-25 15:35:43 +0900

    init:

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Backend
+#Effitizer_Backend
---
+++ start/start/src/test/java/com/effitizer/start/StartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.effitizer.start;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
