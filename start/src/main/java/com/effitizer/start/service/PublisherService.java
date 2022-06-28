8b52b3a6c6f43e8f18d443c536436ff1f1b949b3
HWAGYEONG LEE <dlghkrud18@naver.com>
2022-06-28 17:23:45 +0900

    feat : user controller CRUD, publisher CRU

--- start/src/main/java/com/effitizer/start/service/PublisherService.java
+++ start/src/main/java/com/effitizer/start/service/PublisherService.java
@@ -1,12 +1,17 @@
 package com.effitizer.start.service;
 
+import com.effitizer.start.domain.Contents;
+import com.effitizer.start.domain.Order;
 import com.effitizer.start.domain.Publisher;
+import com.effitizer.start.domain.User;
+import com.effitizer.start.domain.dto.Contents.Request.OnlyContentsRequest;
 import com.effitizer.start.repository.PublisherRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Slf4j
@@ -43,6 +48,19 @@ public class PublisherService {
         return publisher;
     }
 
+    public Publisher editPublisher(String publisherName, Long id) {
+        // 중복 데이터 검사
+         publisherRepository.findByName(publisherName)
+                .ifPresent(p -> {
+                    log.info("CategoryService: 중복 데이터 ---------------------");
+                    throw new IllegalStateException("이미 존재하는 데이터입니다.");
+                });
+         Publisher publisher= publisherRepository.findById(id)
+                .orElseThrow(() -> new IllegalStateException("출판사 정보가 올바르지 않습니다."));
+         publisher.setName(publisherName);
+         return publisher;
+    }
+
     public Publisher savePublisherOrFind(String publisher_name) {
         Optional<Publisher> findPublisher = publisherRepository.findByName(publisher_name);
         if (findPublisher.isEmpty()) {
@@ -52,5 +70,10 @@ public class PublisherService {
         }
         return findPublisher.get();
     }
+
+    public List<Publisher> findAll(){
+        return publisherRepository.findAll();
+    }
+
 
 }
